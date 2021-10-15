d1d8a14373ff82c4dd337fd9fdfc1c450f9f03fb
rodriguezmDNA <joelrome88@gmail.com>
2021-10-15 10:05:47 -0700

    https://www.youtube.com/watch?v=GIF3LaRqgXo&t=1s example

--- setup.py
+++ setup.py
@@ -1,15 +1,15 @@
 from setuptools import setup, find_packages
 
-with open('requirements.txt') as f:
-    required = f.read().splitlines()
+# with open('requirements.txt') as f:
+#     required = f.read().splitlines()
 
-setup(name='testsetup',
-      version='0.0',
-      description='test packaging python code',
+setup(name='helloworld',
+      version='0.0.1',
+      description='say hello!',
       url='https://github.com/rodriguezmDNA/testsetup',
       author='Joel Rodriguez Medina',
       author_email='joelrome88+code@gmail.com',
-      
+      package_dir={'': 'src'},
       packages=find_packages(),
       install_requires=required,
       long_description=open('README.md').read()
--- src/example.py
+++
@@ -1,8 +0,0 @@
-#!usr/bin/env python3
-
-def add_one(number):
-    return number + 1
-
-
-if __name__ == '__main__':
-    print(add_one(3))
