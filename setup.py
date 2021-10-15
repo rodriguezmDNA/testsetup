c927599f5b211b9251336659a785ad734a56bf91
rodriguezmDNA <joelrome88@gmail.com>
2021-10-15 13:14:30 -0700

    update setup, pymodules

--- setup.py
+++ setup.py
@@ -7,13 +7,13 @@ with open('README.md','r') as fh:
     long_desc = fh.read()
 
 setup(name='helloworld',
-      version='0.0',
+      version='0.0.1',
       description='test packaging python code',
       url='https://github.com/rodriguezmDNA/testsetup',
       author='Joel Rodriguez Medina',
       author_email='joelrome88+code@gmail.com',
       package_dir={'': 'src'},
-      py_modules = find_packages(),
+      py_modules = ['helloworld'],
       #packages=find_packages(),
       #install_requires=required,
       long_description=long_desc,
