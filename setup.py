e1e7a8786f46a62a8e4399d835c6ecbf75455879
rodriguezmDNA <joelrome88@gmail.com>
2021-10-15 09:58:21 -0700

    fix setup metadata

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setup(name='testsetup',
       url='https://github.com/rodriguezmDNA/testsetup',
       author='Joel Rodriguez Medina',
       author_email='joelrome88+code@gmail.com',
-      package_dir={'': 'src'},
+      
       packages=find_packages(),
       install_requires=required,
       long_description=open('README.md').read()
