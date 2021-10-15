ac5b0d6362f31ddb7bdb71edf2a6071a7a1be7c4
rodriguezmDNA <joelrome88@gmail.com>
2021-10-15 09:55:01 -0700

    fix src dir

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ setup(name='testsetup',
       author='Joel Rodriguez Medina',
       author_email='joelrome88+code@gmail.com',
       package_dir={'': 'src'},
-      packages=find_packages(where='src'),
+      packages=find_packages(),
       install_requires=required,
       long_description=open('README.md').read()
       )
