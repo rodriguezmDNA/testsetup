37738bf8ddd0ddbbbfecdb2cbfa11d0681bdee59
rodriguezmDNA <joelrome88@gmail.com>
2021-10-15 11:14:07 -0700

    v 1.b devbranch

--- setup.py
+++ setup.py
@@ -1,17 +1,17 @@
 from setuptools import setup, find_packages
 
-# with open('requirements.txt') as f:
-#     required = f.read().splitlines()
+with open('requirements.txt') as f:
+    required = f.read().splitlines()
 
 setup(name='helloworld',
-      version='0.0.1',
+      version='0.0.1b',
       description='say hello!',
       url='https://github.com/rodriguezmDNA/testsetup',
       author='Joel Rodriguez Medina',
       author_email='joelrome88+code@gmail.com',
       package_dir={'': 'src'},
       py_modules=['helloworld']
-      #packages=find_packages(),
-      #install_requires=required,
+      packages=find_packages(),
+      install_requires=required,
       #long_description=open('README.md').read()
       )
