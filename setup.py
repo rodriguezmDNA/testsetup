f5437f7b34327adfff927be6416cbc1aeef17b4c
rodriguezmDNA <joelrome88@gmail.com>
2021-10-15 11:43:54 -0700

    a

--- setup.py
+++ setup.py
@@ -3,8 +3,8 @@ from setuptools import setup, find_packages
 # with open('requirements.txt') as f:
 #     required = f.read().splitlines()
 
-with open('README.md','r') as fh:
-    long_desc = fh.read()
+# with open('README.md','r') as fh:
+#     long_desc = fh.read()
 
 setup(name='helloworld',
       version='0.0.1b',
@@ -13,9 +13,9 @@ setup(name='helloworld',
       author='Joel Rodriguez Medina',
       author_email='joelrome88+code@gmail.com',
       package_dir={'': 'src'},
-      py_modules=['helloworld'],
+      py_modules=['helloworld']
       #packages=find_packages(),
       #install_requires=required,
-      long_description=long_desc,
-      long_description_content_type='text/markdown',
+      #long_description=long_desc,
+      #long_description_content_type='text/markdown',
       )
