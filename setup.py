d421d7ddfb464af85d4ec2398b37de042157ce97
rodriguezmDNA <joelrome88@gmail.com>
2021-10-15 13:18:02 -0700

    fix typo in setup

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-with open('requirements.txt','r`') as f:
+with open('requirements.txt','r') as f:
     required = f.read().splitlines()
 
 with open('README.md','r') as fh:
