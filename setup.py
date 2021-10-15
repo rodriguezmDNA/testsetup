7d3d23dd31931968229a5ca4db218fa02f29c394
rodriguezmDNA <joelrome88@gmail.com>
2021-10-15 13:17:30 -0700

    add seaborn to reqs

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 from setuptools import setup, find_packages
 
-# with open('requirements.txt') as f:
-#     required = f.read().splitlines()
+with open('requirements.txt','r`') as f:
+    required = f.read().splitlines()
 
 with open('README.md','r') as fh:
     long_desc = fh.read()
 
 setup(name='helloworld',
-      version='0.0.1',
+      version='0.0.2',
       description='test packaging python code',
       url='https://github.com/rodriguezmDNA/testsetup',
       author='Joel Rodriguez Medina',
@@ -15,7 +15,7 @@ setup(name='helloworld',
       package_dir={'': 'src'},
       py_modules = ['helloworld'],
       #packages=find_packages(),
-      #install_requires=required,
+      install_requires=required,
       long_description=long_desc,
       long_description_content_type='text/markdown'
       )
