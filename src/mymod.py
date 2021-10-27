19e5069ddbcf678d112d6971f4c67c228e97487b
leonjdaeim <leonjdaeim@cerebro.local>
2021-10-26 22:27:41 -0700

    add new module

---
+++ src/mymod.py
@@ -0,0 +1,10 @@
+def sum(a,b):
+    try:
+        if a is None or b is None:
+            print('need a or b')
+            raise
+        else:
+            return (a+b)
+
+    except:
+        return None
